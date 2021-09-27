6715d9b1b1491276acaaa7d1e41c1d76dd787044
Juan Mejía <juanes.reich@gmail.com>
2021-09-27 15:24:09 -0500

    Adding new sounds horses

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
+let MAIN_MUSIC = new Audio('recursos/sounds/mainSound.mp3')
 let POP_SOUND = new Audio('recursos/sounds/pop.mp3')
-let BLADE_BTN = new Audio('recursos/sounds/blade_btn.mp3')
+let SWORD_SOUND = new Audio('recursos/sounds/blade_btn.mp3')
+let HORSE_SOUND = new Audio('recursos/sounds/horse_btn.mp3')
 
-function btn_sound() {
-    BLADE_BTN.autoplay = true;
-    BLADE_BTN.volume = 0.05;
-    BLADE_BTN.load();
+function mainSound() {
+    MAIN_MUSIC.autoplay = true;
+    MAIN_MUSIC.volume = 0.07;
+    MAIN_MUSIC.load();
 }
 
+function sword_btn() {
+    SWORD_SOUND.autoplay = true;
+    SWORD_SOUND.volume = 0.05;
+    SWORD_SOUND.load();
+}
+
+function horse_btn() {
+    HORSE_SOUND.autoplay = true;
+    HORSE_SOUND.volume = 0.05;
+    HORSE_SOUND.load();
+}
+
+function pop_btn() {
+    POP_SOUND.autoplay = true;
+    POP_SOUND.volume = 0.05;
+    POP_SOUND.load();
+}
+window.onload = mainSound();
