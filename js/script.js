11fe7fba0fffc65a0470fab679dd5c9631d72c6d
Juan Mejía <juanes.reich@gmail.com>
2021-09-27 12:21:17 -0500

    adding sounds

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let POP_SOUND = new Audio('recursos/sounds/pop.mp3')
+let BLADE_BTN = new Audio('recursos/sounds/blade_btn.mp3')
+
+function btn_sound() {
+    BLADE_BTN.autoplay = true;
+    BLADE_BTN.volume = 0.05;
+    BLADE_BTN.load();
+}
+
