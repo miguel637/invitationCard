ee0529a75b62e0d7072b778a97d09c5c193e7a6d
Juan Mejía <juanes.reich@gmail.com>
2021-09-28 14:06:07 -0500

    New music styles v7.8

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ let TRUMPET_SOUND = new Audio('recursos/sounds/trumpet_btn.mp3');
 let SWORD_SOUND = new Audio('recursos/sounds/blade_btn.mp3');
 let HORSE_SOUND = new Audio('recursos/sounds/horse_btn.mp3');
 
-let shrekSound = new Audio('recursos/sounds/Shrek/shrek'+getRandomInt(1, 11)+'.mp3');
-let disneySound = new Audio('recursos/sounds/Disney/disney'+getRandomInt(1, 9)+'.mp3');
-let grimmSound = new Audio('recursos/sounds/Grimm/grimm'+getRandomInt(1, 3)+'.mp3');
-let medievalSound = new Audio('recursos/sounds/Medieval/medieval'+getRandomInt(1, 6)+'.mp3');
+let shrekSound = new Audio('../recursos/sounds/Shrek/shrek'+getRandomInt(1, 11)+'.mp3');
+let disneySound = new Audio('../recursos/sounds/Disney/disney'+getRandomInt(1, 9)+'.mp3');
+let grimmSound = new Audio('../recursos/sounds/Grimm/grimm'+getRandomInt(1, 3)+'.mp3');
+let medievalSound = new Audio('../recursos/sounds/Medieval/medieval'+getRandomInt(1, 6)+'.mp3');
 
 let maxVolume = 0.07;
 let minVolume = 0.008;
