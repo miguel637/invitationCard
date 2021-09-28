94d4967cc8b025f55d7c8ea847eb9481e168700f
Juan Mejía <juanes.reich@gmail.com>
2021-09-28 12:06:26 -0500

    New music styles v7

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let MAIN_MUSIC = new Audio('recursos/sounds/mainSound.mp3');
-let POP_SOUND = new Audio('recursos/sounds/pop.mp3');
+let TRUMPET_SOUND = new Audio('recursos/sounds/trumpet_btn.mp3');
 let SWORD_SOUND = new Audio('recursos/sounds/blade_btn.mp3');
 let HORSE_SOUND = new Audio('recursos/sounds/horse_btn.mp3');
 
@@ -8,18 +8,21 @@ let disneySound = new Audio('recursos/sounds/Disney/disney'+getRandomInt(1, 9)+'
 let grimmSound = new Audio('recursos/sounds/Grimm/grimm'+getRandomInt(1, 3)+'.mp3');
 let medievalSound = new Audio('recursos/sounds/Medieval/medieval'+getRandomInt(1, 6)+'.mp3');
 
-let mainValue = 0.07;
+let maxVolume = 0.07;
+let minVolume = 0.008;
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 function esc_btn() {
     shrekSound.pause();
     disneySound.pause();
-    MAIN_MUSIC.volume = mainValue;
+    grimmSound.pause();
+    medievalSound.pause();
+    MAIN_MUSIC.volume = maxVolume;
 }
 function mainSound() {
     MAIN_MUSIC.autoplay = true;
-    MAIN_MUSIC.volume = mainValue;
+    MAIN_MUSIC.volume = maxVolume;
     MAIN_MUSIC.loop = true;
     MAIN_MUSIC.load();
 }
@@ -33,6 +36,11 @@ function horse_btn() {
     HORSE_SOUND.volume = 0.05;
     HORSE_SOUND.load();
 }
+function trumpet_btn() {
+    TRUMPET_SOUND.autoplay = true;
+    TRUMPET_SOUND.volume = 0.05;
+    TRUMPET_SOUND.load();
+}
 function pauseAll(){
     shrekSound.pause();
     disneySound.pause();
@@ -41,7 +49,7 @@ function pauseAll(){
 }
 function shrek_btn() {
     pauseAll();
-    MAIN_MUSIC.volume = 0.01;
+    MAIN_MUSIC.volume = minVolume;
     shrekSound = new Audio('recursos/sounds/Shrek/shrek'+getRandomInt(1, 11)+'.mp3');
     shrekSound.autoplay = true;
     shrekSound.volume = 0.8;
@@ -49,7 +57,7 @@ function shrek_btn() {
 }
 function disney_btn() {
     pauseAll();
-    MAIN_MUSIC.volume = 0.01;
+    MAIN_MUSIC.volume = minVolume;
     disneySound = new Audio('recursos/sounds/Disney/disney'+getRandomInt(1, 9)+'.mp3');
     disneySound.autoplay = true;
     disneySound.volume = 0.7;
@@ -57,23 +65,18 @@ function disney_btn() {
 }
 function grimm_btn() {
     pauseAll();
-    MAIN_MUSIC.volume = 0.01;
+    MAIN_MUSIC.volume = minVolume;
     grimmSound = new Audio('recursos/sounds/Grimm/grimm'+getRandomInt(1, 3)+'.mp3');
     grimmSound.autoplay = true;
-    grimmSound.volume = 0.7;
+    grimmSound.volume = 0.4;
     grimmSound.load();
 }
-
 function medieval_btn() {
     pauseAll();
-    MAIN_MUSIC.volume = 0.01;
+    MAIN_MUSIC.volume = minVolume;
     medievalSound = new Audio('recursos/sounds/Medieval/medieval'+getRandomInt(1, 6)+'.mp3');
     medievalSound.autoplay = true;
-    medievalSound.volume = 0.8;
+    medievalSound.volume = 0.7;
     medievalSound.load();
-}
-
-function pop_btn() {
-    console.log(MAIN_MUSIC.volume);
 }
 window.onload = mainSound;
