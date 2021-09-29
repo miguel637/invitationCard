ece84fd628abb9b5da89ecb64f1e2b5d13edc0fc
Juan Mejía <juanes.reich@gmail.com>
2021-09-29 10:10:25 -0500

    new model v2

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
 let URL = "https://miguel637.github.io/invitationCard/";
-let MAIN_MUSIC = new Audio(src=URL+"recursos/sounds/mainSound.mp3");
-let TRUMPET_SOUND = new Audio(src=URL+"recursos/sounds/trumpet_btn.mp3");
-let SWORD_SOUND = new Audio(src=URL+"recursos/sounds/blade_btn.mp3");
-let HORSE_SOUND = new Audio(src=URL+"recursos/sounds/horse_btn.mp3");
-
-let shrekSound = new Audio(URL+"recursos/sounds/Shrek/Shrek"+getRandomInt(1, 11)+".mp3");
-let disneySound = new Audio(src=URL+"recursos/sounds/Disney/disney"+getRandomInt(1, 9)+".mp3");
-let grimmSound = new Audio(src=URL+"recursos/sounds/Grimm/grimm"+getRandomInt(1, 3)+".mp3");
-let medievalSound = new Audio(src=URL+"recursos/sounds/Medieval/medieval"+getRandomInt(1, 6)+".mp3");
+let MAIN_MUSIC = new Audio("recursos/sounds/mainSound.mp3");
+let TRUMPET_SOUND = new Audio("recursos/sounds/trumpet_btn.mp3");
+let SWORD_SOUND = new Audio("recursos/sounds/blade_btn.mp3");
+let HORSE_SOUND = new Audio("recursos/sounds/horse_btn.mp3");
+let lastNum=0;
+let shrekSound = new Audio("recursos/sounds/Shrek/Shrek"+getRandomInt(1, 11)+".mp3");
+let disneySound = new Audio("recursos/sounds/Disney/disney"+getRandomInt(1, 9)+".mp3");
+let grimmSound = new Audio("recursos/sounds/Grimm/grimm"+getRandomInt(1, 3)+".mp3");
+let medievalSound = new Audio("recursos/sounds/Medieval/medieval"+getRandomInt(1, 6)+".mp3");
 
 let maxVolume = 0.07;
 let minVolume = 0.008;
+
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+    let randomNum = Math.floor(Math.random() * (max - min)) + min;
+    while (randomNum === lastNum){
+        randomNum= Math.floor(Math.random() * (max - min)) + min;
+    }
+    lastNum = randomNum;
+    return randomNum;
 }
 function esc_btn() {
     shrekSound.pause();
@@ -51,7 +57,7 @@ function pauseAll(){
 function shrek_btn() {
     pauseAll();
     MAIN_MUSIC.volume = minVolume;
-    shrekSound = new Audio(src=URL+"recursos/sounds/Shrek/Shrek"+getRandomInt(1, 11)+".mp3");
+    shrekSound = new Audio("recursos/sounds/Shrek/Shrek"+getRandomInt(1, 11)+".mp3");
     shrekSound.autoplay = true;
     shrekSound.volume = 0.8;
     shrekSound.load();
@@ -59,7 +65,7 @@ function shrek_btn() {
 function disney_btn() {
     pauseAll();
     MAIN_MUSIC.volume = minVolume;
-    disneySound = new Audio(src=URL+"recursos/sounds/Disney/disney"+getRandomInt(1, 9)+".mp3");
+    disneySound = new Audio("recursos/sounds/Disney/disney"+getRandomInt(1, 9)+".mp3");
     disneySound.autoplay = true;
     disneySound.volume = 0.6;
     disneySound.load();
@@ -67,7 +73,7 @@ function disney_btn() {
 function grimm_btn() {
     pauseAll();
     MAIN_MUSIC.volume = minVolume;
-    grimmSound = new Audio(src=URL+"recursos/sounds/Grimm/grimm"+getRandomInt(1, 3)+".mp3");
+    grimmSound = new Audio("recursos/sounds/Grimm/grimm"+getRandomInt(1, 3)+".mp3");
     grimmSound.autoplay = true;
     grimmSound.volume = 0.4;
     grimmSound.load();
@@ -75,7 +81,7 @@ function grimm_btn() {
 function medieval_btn() {
     pauseAll();
     MAIN_MUSIC.volume = minVolume;
-    medievalSound = new Audio(src=URL+"recursos/sounds/Medieval/medieval"+getRandomInt(1, 6)+".mp3");
+    medievalSound = new Audio("recursos/sounds/Medieval/medieval"+getRandomInt(1, 6)+".mp3");
     medievalSound.autoplay = true;
     medievalSound.volume = 0.6;
     medievalSound.load();
